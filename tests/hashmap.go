cf0de731ccaffb60f84c0ee860c2e4c1ebd3de51
hongmaoxiao <769501294@qq.com>
2017-07-20 07:29:27 +0800

    - added another test - added script to run tests - fixed primary expression syntax in go.atg

--- tests/hashmap.go
+++ tests/hashmap.go
@@ -17,7 +17,6 @@ func ASSERT(p bool) {
 	}
 }
 
-
 // ----------------------------------------------------------------------------
 // Implementation of the HashMap
 
@@ -26,17 +25,14 @@ type KeyType interface {
 	Match(other *KeyType) bool
 }
 
-
 type ValueType interface {
 	// empty interface
 }
-
 
 type Entry struct {
 	key   *KeyType
 	value *ValueType
 }
-
 
 // Using the Array type below doesn't seem to work
 //type Array array [1024] Entry;
@@ -47,11 +43,9 @@ type HashMap struct {
 	occupancy_     uint32
 }
 
-
 func (m *HashMap) capacity() uint32 {
 	return 1 << m.log2_capacity_
 }
-
 
 func (m *HashMap) Clear() {
 	// Mark all entries as empty.
@@ -63,13 +57,11 @@ func (m *HashMap) Clear() {
 	m.occupancy_ = 0
 }
 
-
 func (m *HashMap) Initialize(initial_log2_capacity uint32) {
 	m.log2_capacity_ = initial_log2_capacity
 	m.map_ = new([1024]Entry)
 	m.Clear()
 }
-
 
 func (m *HashMap) Probe(key *KeyType) *Entry {
 	ASSERT(key != nil)
@@ -88,9 +80,7 @@ func (m *HashMap) Probe(key *KeyType) *Entry {
 	return &m.map_[i]
 }
 
-
 func (m *HashMap) Resize()
-
 
 func (m *HashMap) Lookup(key *KeyType, insert bool) *Entry {
 	// Find a matching entry.
@@ -118,71 +108,63 @@ func (m *HashMap) Lookup(key *KeyType, insert bool) *Entry {
 	return nil
 }
 
-
 func (m *HashMap) Resize() {
-	var hmap *[1024] Entry = m.map_;
-	var n uint32 = m.occupancy_;
+	var hmap *[1024]Entry = m.map_
+	var n uint32 = m.occupancy_
 
 	// Allocate a new map of twice the current size.
-	m.Initialize(m.log2_capacity_ << 1);
+	m.Initialize(m.log2_capacity_ << 1)
 
 	// Rehash all current entries.
-	var i uint32 = 0;
+	var i uint32 = 0
 	for n > 0 {
 		if hmap[i].key != nil {
-			m.Lookup(hmap[i].key, true).value = hmap[i].value;
-			n = n - 1;
+			m.Lookup(hmap[i].key, true).value = hmap[i].value
+			n = n - 1
 		}
-		i++;
+		i++
 	}
 }
-
 
 // ----------------------------------------------------------------------------
 // Test code
 
 type Number struct {
-	x uint32;
+	x uint32
 }
-
 
 func (n *Number) Hash() uint32 {
-	return n.x * 23;
+	return n.x * 23
 }
-
 
 func (n *Number) Match(other *KeyType) bool {
 	// var y *Number = other;
 	// return n.x = y.x;
-	return false;
+	return false
 }
-
 
 func MakeNumber(x uint32) *Number {
-	var n *Number = new(Number);
-	n.x = x;
-	return n;
+	var n *Number = new(Number)
+	n.x = x
+	return n
 }
 
-
 func main() {
-	func (n int) int {
-		return n + 1;
-	}(1);
+	// func (n int) int { return n + 1; }(1);
 
-    print "HashMap - gri 2/8/2008\n";
+	// print "HashMap - gri 2/8/2008\n";
 
-	var hmap *HashMap = new(HashMap);
-	hmap.Initialize(0);
+	var hmap *HashMap = new(HashMap)
+	hmap.Initialize(0)
 
-	var x1 *Number = MakeNumber(1001);
-	var x2 *Number = MakeNumber(2002);
-	var x3 *Number = MakeNumber(3003);
+	var x1 *Number = MakeNumber(1001)
+	var x2 *Number = MakeNumber(2002)
+	var x3 *Number = MakeNumber(3003)
 
 	// this doesn't work I think...
 	//hmap.Lookup(x1, true);
 	//hmap.Lookup(x2, true);
 	//hmap.Lookup(x3, true);
 
-    print "done\n";
+	// print "done\n";
 }
